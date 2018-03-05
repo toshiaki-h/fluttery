54e23587679cf9629c3ba511497a6a86cd4f81a1
Matt Carroll <me@mattcarroll.name>
2018-03-04 19:50:49 -0800

    Added example project to Fluttery package.

---
+++ example/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:example/example_framing.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Fluttery Example',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Fluttery'),
+      ),
+      body: new FramingExampleScreen(
+        
+      )
+    );
+  }
+}
